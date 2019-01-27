b1042def9c3e67ea1cd8ae334ac2cd6988948c7d
Andrew <healeycodes@gmail.com>
2019-01-27 10:44:27 +0000

    Add more tests, header image

--- bench.py
+++ bench.py
@@ -10,13 +10,13 @@ parser = argparse.ArgumentParser(description='Benchmark a WebSocket server')
 parser.add_argument('--h', dest='host',
                     help='Host address of WebSocket server', default='localhost:3000')
 parser.add_argument('--n', dest='clients',
-                    help='Number of clients to create', default=1000)
+                    help='Number of clients to create', default=1000, type=int)
 parser.add_argument('--c', dest='concurrency',
-                    help='Number of concurrent clients', default=64)
+                    help='Number of concurrent clients', default=64, type=int)
 parser.add_argument('--r', dest='roundtrips',
-                    help='Roundtrips per client', default=5)
+                    help='Roundtrips per client', default=5, type=int)
 parser.add_argument('--s', dest='msg_size',
-                    help='Message size in characters', default=30)
+                    help='Message size in characters', default=30, type=int)
 parser.add_argument('--l', dest='log_path',
                     help='Path to create or append to a log file', default=os.path.join('.', 'log.txt'))
 args = parser.parse_args()
@@ -30,7 +30,7 @@ log_file = open(args.log_path, 'a')
 log_memory = list()
 
 print(f'Benchmarking {host} with {clients} total clients. ' +
-      f'{concurrency} clients concurrently. {roundtrips} roundtrips per client.')
+      f'{concurrency} clients concurrently. {roundtrips} roundtrips per client.\n')
 
 
 async def client(state):
@@ -83,3 +83,4 @@ def stats(timings):
 
 
 stats(log_memory)
+print(f'\nRaw results sent to {log_file.name}')
--- test_bench.py
+++ test_bench.py
@@ -2,28 +2,52 @@
 
 import unittest
 import subprocess
+import warnings
 
 # python environmental variable
-python = 'python'
+python_env = 'python'
+
+
+def ignore_resource_warning(func):
+    '''Ignore any resource warnings produced by leaving echo_server running.'''
+
+    def without_warn(self, *args, **kwargs):
+        warnings.simplefilter("ignore", ResourceWarning)
+        return func(self, *args, **kwargs)
+    return without_warn
+
 
 class EndToEnd(unittest.TestCase):
-
     def test_without_echo_server(self):
-        echo_server = subprocess.Popen([python, 'echo_server.py', '--n', '64'])
-        benchmarker = subprocess.Popen(
-            [python, 'bench.py'], stdout=subprocess.DEVNULL)
-        benchmarker.wait()
-        echo_server.kill()
-        assert(benchmarker.returncode == 1)
+        '''The benchmark should error without the presense of an echo_server on `host` arg.'''
 
+        result = subprocess.run(
+            [python_env, 'bench.py'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        assert(result.returncode == 1)
 
+    @ignore_resource_warning
     def test_with_echo_server(self):
-        echo_server = subprocess.Popen([python, 'echo_server.py', '--n', '64'])
-        benchmarker = subprocess.Popen(
-            [python, 'bench.py'], stdout=subprocess.DEVNULL)
-        benchmarker.wait()
+        '''The benchmark should run without errors.'''
+
+        echo_server = subprocess.Popen([python_env, 'echo_server.py', '--n', '64'])
+        result = subprocess.run(
+            [python_env, 'bench.py'], stdout=subprocess.DEVNULL)
         echo_server.kill()
-        assert(benchmarker.returncode == 0)
+        assert(result.returncode == 0)
+
+    @ignore_resource_warning
+    def test_stat_report(self):
+        '''Statistics of the benchmark should be printed to stdout.'''
+
+        echo_server = subprocess.Popen([python_env, 'echo_server.py', '--n', '64'])
+        result = subprocess.Popen([python_env, 'bench.py'], stdout=subprocess.PIPE, shell=True)
+        result.wait()
+        output = str(result.stdout.read())
+        echo_server.kill()
+        assert('Min' in output)
+        assert('Mean' in output)
+        assert('Max' in output)
+
 
 if __name__ == '__main__':
     unittest.main()
