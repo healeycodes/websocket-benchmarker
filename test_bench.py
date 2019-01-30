df3792b19449030d1cb82d63a932d541793807a5
Andrew <healeycodes@gmail.com>
2019-01-30 21:00:35 +0000

    test shell alternative

--- test_bench.py
+++ test_bench.py
@@ -20,18 +20,18 @@ class EndToEnd(unittest.TestCase):
     def test_without_echo_server(self):
         '''The benchmark should error without the presense of an echo_server on `host` arg.'''
 
-        result = subprocess.run(
-            [python_env, 'bench.py'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run([python_env, 'bench.py'],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         assert(result.returncode == 1)
 
     @ignore_resource_warning
     def test_with_echo_server(self):
         '''The benchmark should run without errors.'''
 
-        echo_server = subprocess.Popen(
-            [python_env, 'echo_server.py', '--n', '64'])
-        result = subprocess.run(
-            [python_env, 'bench.py'], stdout=subprocess.DEVNULL)
+        echo_server = subprocess.Popen([python_env, 'echo_server.py', '--n', '64'],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        result = subprocess.run([python_env, 'bench.py'],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         echo_server.kill()
         assert(result.returncode == 0)
 
@@ -39,10 +39,10 @@ class EndToEnd(unittest.TestCase):
     def test_stat_report(self):
         '''Statistics of the benchmark should be printed to stdout.'''
 
-        echo_server = subprocess.Popen(
-            [python_env, 'echo_server.py', '--n', '64'])
-        result = subprocess.Popen(
-            [python_env, 'bench.py'], stdout=subprocess.PIPE, shell=True)
+        echo_server = subprocess.Popen([python_env, 'echo_server.py', '--n', '64'],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        result = subprocess.Popen([python_env, 'bench.py'],
+            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
         result.wait()
         output = str(result.stdout.read())
         echo_server.kill()
